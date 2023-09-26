48b1d521352922c24d918a311dcfb9eeb423a7c2
Dheeraj R Shrivatsa <97652816+Dheeraj3031A@users.noreply.github.com>
2023-09-26 16:03:36 +0530

    Bump to v4.22

--- config/version.mk
+++ config/version.mk
@@ -11,7 +11,7 @@ PRODUCT_PACKAGES += \
 
 TARGET_PRODUCT_SHORT := $(subst havoc_,,$(HAVOC_BUILD_TYPE))
 
-HAVOC_VERSION = 4.21
+HAVOC_VERSION = 4.22
 HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y)
 HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)/$(shell date -u +%H%M)
 
